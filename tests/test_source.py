4294a639c4a8033fa80828452987b996c7efebbd
Eric Casteleijn <thisfred@gmail.com>
2017-05-27 10:46:16 -0700

    added test for source comparison

---
+++ tests/test_source.py
@@ -0,0 +1,10 @@
+from breakfast.source import Source
+
+
+def test_ordering():
+    source1 = Source(lines=None, module_name='foo')
+    source2 = Source(lines=None, module_name='bar')
+    assert source1 > source2
+    assert source1 != source2
+    assert source2 <= source1
+    assert source2 == source2
