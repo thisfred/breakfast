724bad2f82931f2f0794a2ce76336917909ef151
Eric Casteleijn <thisfred@gmail.com>
2017-06-05 23:08:19 -0700

    move stuff around

--- breakfast/names.py
+++ breakfast/names.py
@@ -22,6 +22,14 @@ class Collector:
         self._in_class = False
 
     @contextmanager
+    def namespaces(self, names):
+        for name in names:
+            self._enter_namespace(name)
+        yield
+        for _ in names:
+            self._leave_namespace()
+
+    @contextmanager
     def enter_class(self, name):
         old = self._in_class
         self._in_class = True
@@ -43,57 +51,17 @@ class Collector:
 
     @contextmanager
     def namespace(self, name):
-        self.enter_namespace(name)
+        self._enter_namespace(name)
         yield
-        self.leave_namespace()
+        self._leave_namespace()
 
     def get_positions(self, position):
-        self.post_process()
+        self._post_process()
         for name, positions in self._occurrences.items():
             if (name in self._definitions and
                     any(position == p for p in positions)):
                 return sorted(positions)
-
         return []
-
-    def rewrite_selves(self):
-        for name in self._occurrences.copy():
-            for alias in self._class_aliases:
-                if is_prefix(alias, name):
-                    new_name = (self._class_aliases[alias] + name[len(alias):])
-                    self._occurrences[new_name].extend(self._occurrences[name])
-                    del self._occurrences[name]
-                    if name in self._definitions:
-                        self._definitions[new_name].extend(
-                            self._definitions[name])
-                        del self._definitions[name]
-
-    def post_process(self):
-        self.rewrite_selves()
-        for name in self._occurrences.copy():
-
-            if name not in self._definitions:
-                rewritten = self.rewrite(name)
-                if rewritten in self._definitions:
-                    self._occurrences[rewritten] += self._occurrences[name]
-                else:
-                    definition = self.get_from_outer_scope(name)
-                    if definition and definition in self._definitions:
-                        self._occurrences[
-                            definition] += self._occurrences[name]
-
-                del self._occurrences[name]
-
-    def get_from_outer_scope(self, name):
-        for scope in self._scopes:
-            if is_prefix(scope, name):
-                return scope[:-1] + name[len(scope):]
-
-    def enter_namespace(self, name):
-        self._current_namespace = self.get_namespaced(name)
-
-    def leave_namespace(self):
-        self._current_namespace = self._current_namespace[:-1]
 
     def add_scope(self, name, is_method=False):
         if is_method:
@@ -116,7 +84,51 @@ class Collector:
     def add_alias(self, alias, name):
         self._aliases[self.get_namespaced(name)] = self.get_namespaced(alias)
 
-    def rewrite(self, full_name):
+    def add_superclass(self, subclass, superclass):
+        superclass = self.get_namespaced(superclass)
+        if superclass in self._imports:
+            superclass = self._imports[superclass]
+        self._superclasses[self.get_namespaced(subclass)].append(superclass)
+
+    def add_class_alias(self, alias):
+        full_name = self.get_namespaced(alias)
+        self._class_aliases[full_name] = full_name[:-2]
+
+    def add_import(self, name, full_name):
+        self._imports[self.get_namespaced(name)] = full_name
+
+    def add_class(self, name):
+        self._classes.add(self.get_namespaced(name))
+
+    def _post_process(self):
+        self._rewrite_selves()
+        for name in self._occurrences.copy():
+            if name not in self._definitions:
+                rewritten = self._rewrite(name)
+                if rewritten in self._definitions:
+                    self._occurrences[rewritten] += self._occurrences[name]
+                else:
+                    definition = self._get_from_outer_scope(name)
+                    if definition and definition in self._definitions:
+                        self._occurrences[
+                            definition] += self._occurrences[name]
+                del self._occurrences[name]
+
+    def _get_from_outer_scope(self, name):
+        for scope in self._scopes:
+            if is_prefix(scope, name):
+                return scope[:-1] + name[len(scope):]
+
+    def _enter_namespace(self, name):
+        self._current_namespace = self.get_namespaced(name)
+
+    def _leave_namespace(self):
+        self._current_namespace = self._current_namespace[:-1]
+
+    def _add_namespaced_occurrence(self, full_name, position):
+        self._occurrences[full_name].append(position)
+
+    def _rewrite(self, full_name):
         if full_name in self._imports:
             return self._imports[full_name]
 
@@ -127,19 +139,25 @@ class Collector:
 
         if (full_name not in self._definitions and
                 full_name[:-1] in self._classes):
-            inherited = self.get_inherited_definition(full_name)
+            inherited = self._get_inherited_definition(full_name)
             if inherited:
                 return inherited
 
         return full_name
 
-    def add_superclass(self, subclass, superclass):
-        superclass = self.get_namespaced(superclass)
-        if superclass in self._imports:
-            superclass = self._imports[superclass]
-        self._superclasses[self.get_namespaced(subclass)].append(superclass)
+    def _rewrite_selves(self):
+        for name in self._occurrences.copy():
+            for alias in self._class_aliases:
+                if is_prefix(alias, name):
+                    new_name = (self._class_aliases[alias] + name[len(alias):])
+                    self._occurrences[new_name].extend(self._occurrences[name])
+                    del self._occurrences[name]
+                    if name in self._definitions:
+                        self._definitions[new_name].extend(
+                            self._definitions[name])
+                        del self._definitions[name]
 
-    def get_inherited_definition(self, full_name):
+    def _get_inherited_definition(self, full_name):
         cls = full_name[:-1]
         if cls not in self._superclasses:
             return
@@ -164,27 +182,12 @@ class Collector:
             new_supers = self._superclasses[cls]
             supers.extend(new_supers)
 
-    def _add_namespaced_occurrence(self, full_name, position):
-        self._occurrences[full_name].append(position)
-
-    def add_class_alias(self, alias):
-        full_name = self.get_namespaced(alias)
-        self._class_aliases[full_name] = full_name[:-2]
-
-    def add_import(self, name, full_name):
-        self._imports[self.get_namespaced(name)] = full_name
-
-    def add_class(self, name):
-        self._classes.add(self.get_namespaced(name))
-
 
 class Names(NodeVisitor):
 
     def __init__(self):
         self.current_source = None
         self.collector = Collector()
-        self._in_class = False
-        self.in_method = False
 
     def visit_source(self, source):
         self.current_source = source
@@ -193,35 +196,26 @@ class Names(NodeVisitor):
     def get_occurrences(self, _, position):
         return self.collector.get_positions(position)
 
-    def add_occurrence(self, name, position):
-        self.collector.add_occurrence(name, position)
-
-    def add_definition(self, name, position):
-        self.collector.add_definition(name, position)
-
-    def add_alias(self, alias, name):
-        self.collector.add_alias(alias, name)
-
     def visit_Module(self, node):  # noqa
         with self.collector.namespace(self.current_source.module_name):
             self.generic_visit(node)
 
     def visit_Name(self, node):  # noqa
         if self._is_definition(node):
-            action = self.add_definition
+            action = self.collector.add_definition
         else:
-            action = self.add_occurrence
-        position = self.position_from_node(node)
+            action = self.collector.add_occurrence
+        position = self._position_from_node(node)
         name = node.id
         action(name, position=position)
 
     def visit_ClassDef(self, node):  # noqa
-        position = self.position_from_node(
+        position = self._position_from_node(
             node=node,
             row_offset=len(node.decorator_list),
             column_offset=len("class "))
         self.collector.add_class(node.name)
-        self.add_definition(
+        self.collector.add_definition(
             name=node.name,
             position=position)
         for base in node.bases:
@@ -232,75 +226,45 @@ class Names(NodeVisitor):
                 self.visit(statement)
 
     def visit_FunctionDef(self, node):  # noqa
-        position = self.position_from_node(
+        position = self._position_from_node(
             node=node,
             row_offset=len(node.decorator_list),
             column_offset=len("def "))
-        self.add_definition(
+        self.collector.add_definition(
             name=node.name,
             position=position)
         with self.collector.enter_function(node.name):
             for i, arg in enumerate(node.args.args):
                 if i == 0 and self.collector.in_method:
                     # this is 'self' in a method definition
-                    self.add_self(arg)
-                self.add_parameter(arg)
+                    self._add_self(arg)
+                self._add_parameter(arg)
             self.generic_visit(node)
 
-    def add_self(self, arg):
-        if isinstance(arg, Name):
-            # python 2
-            alias = arg.id
-        else:
-            alias = arg.arg
-        self.collector.add_class_alias(alias)
-
-    def add_parameter(self, arg):
-        if isinstance(arg, Name):
-            # python 2: these will be caught by generic_visit
-            return
-
-        # python 3
-        position = self.position_from_node(arg)
-        self.add_definition(
-            name=arg.arg,
-            position=position)
-
-    def names_from(self, node):
-        if isinstance(node, Name):
-            return (node.id,)
-
-        if isinstance(node, Attribute):
-            return self.names_from(node.value) + (node.attr,)
-
     def visit_Call(self, node):  # noqa
-        start = self.position_from_node(node)
+        start = self._position_from_node(node)
         self.visit(node.func)
-        names = self.names_from(node.func)
-        for name in names:
-            position = self.current_source.find_after(name, start)
-            self.collector.enter_namespace(name)
-        for keyword in node.keywords:
-            position = self.current_source.find_after(keyword.arg, start)
-            self.collector.add_occurrence(
-                name=keyword.arg,
-                position=position)
-        for _ in names:
-            self.collector.leave_namespace()
+        names = self._names_from(node.func)
+        with self.collector.namespaces(names):
+            for keyword in node.keywords:
+                position = self.current_source.find_after(keyword.arg, start)
+                self.collector.add_occurrence(
+                    name=keyword.arg,
+                    position=position)
         for arg in node.args:
             self.visit(arg)
         for keyword in node.keywords:
             self.visit(keyword.value)
 
     def visit_Import(self, node):  # noqa
-        start = self.position_from_node(node)
+        start = self._position_from_node(node)
         for alias in node.names:
             name = alias.name
             position = self.current_source.find_after(name, start)
-            self.add_occurrence(name, position)
+            self.collector.add_occurrence(name, position)
 
     def visit_ImportFrom(self, node):  # noqa
-        start = self.position_from_node(node)
+        start = self._position_from_node(node)
         for imported in node.names:
             name = imported.name
             full_name = (node.module, name)
@@ -309,27 +273,24 @@ class Names(NodeVisitor):
             self.collector.add_occurrence(name, position)
 
     def visit_Attribute(self, node):  # noqa
-        start = self.position_from_node(node)
+        start = self._position_from_node(node)
         self.visit(node.value)
-        names = self.names_from(node.value)
-        for name in names:
-            self.collector.enter_namespace(name)
-        name = node.attr
-        position = self.current_source.find_after(name, start)
-        if self._is_definition(node):
-            self.add_definition(name, position)
-        else:
-            self.collector.add_occurrence(name, position)
-        for _ in names:
-            self.collector.leave_namespace()
+        names = self._names_from(node.value)
+        with self.collector.namespaces(names):
+            name = node.attr
+            position = self.current_source.find_after(name, start)
+            if self._is_definition(node):
+                self.collector.add_definition(name, position)
+            else:
+                self.collector.add_occurrence(name, position)
 
     def visit_Assign(self, node):  # noqa
-        target_names = self.get_names(node.targets[0])
-        value_names = self.get_names(node.value)
+        target_names = self._get_names(node.targets[0])
+        value_names = self._get_names(node.value)
         self.generic_visit(node)
         for target, value in zip(target_names, value_names):
             if target and value:
-                self.add_alias(value, target)
+                self.collector.add_alias(value, target)
 
     def visit_DictComp(self, node):  # noqa
         self._comp_visit(node, node.key, node.value)
@@ -341,7 +302,7 @@ class Names(NodeVisitor):
         self._comp_visit(node, node.elt)
 
     def _comp_visit(self, node, *rest):
-        position = self.position_from_node(node)
+        position = self._position_from_node(node)
         # The dashes make sure it can never clash with an actual Python name.
         name = 'comprehension-%s-%s' % (position.row, position.column)
         # ns = self.current.add_namespace(
@@ -356,7 +317,33 @@ class Names(NodeVisitor):
             for sub_node in rest:
                 self.visit(sub_node)
 
-    def position_from_node(self, node, row_offset=0, column_offset=0):
+    def _add_self(self, arg):
+        if isinstance(arg, Name):
+            # python 2
+            alias = arg.id
+        else:
+            alias = arg.arg
+        self.collector.add_class_alias(alias)
+
+    def _add_parameter(self, arg):
+        if isinstance(arg, Name):
+            # python 2: these will be caught by generic_visit
+            return
+
+        # python 3
+        position = self._position_from_node(arg)
+        self.collector.add_definition(
+            name=arg.arg,
+            position=position)
+
+    def _names_from(self, node):
+        if isinstance(node, Name):
+            return (node.id,)
+
+        if isinstance(node, Attribute):
+            return self._names_from(node.value) + (node.attr,)
+
+    def _position_from_node(self, node, row_offset=0, column_offset=0):
         return Position(
             source=self.current_source,
             row=(node.lineno - 1) + row_offset,
@@ -366,13 +353,13 @@ class Names(NodeVisitor):
     def _is_definition(node):
         return isinstance(node.ctx, (Param, Store))
 
-    def get_names(self, value):
+    def _get_names(self, value):
         if isinstance(value, Tuple):
-            return [self.get_value_name(v) for v in value.elts]
+            return [self._get_value_name(v) for v in value.elts]
 
-        return [self.get_value_name(value)]
+        return [self._get_value_name(value)]
 
-    def get_value_name(self, value):
+    def _get_value_name(self, value):
         if isinstance(value, Attribute):
             return value.attr
 
@@ -380,7 +367,7 @@ class Names(NodeVisitor):
             return value.id
 
         if isinstance(value, Call):
-            return self.get_value_name(value.func)
+            return self._get_value_name(value.func)
 
         return None
 
