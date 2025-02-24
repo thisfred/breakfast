e77ef1a47f63081acd7a4ed9fcbf9e4c49ad0ff9
Eric Casteleijn <thisfred@gmail.com>
2025-02-23 17:35:07 -0800

    sort __all__

--- src/breakfast/__init__.py
+++ src/breakfast/__init__.py
@@ -4,4 +4,4 @@ from breakfast.project import Project
 from breakfast.source import Position, Source
 
 __version__ = "2024.03.10.1"
-__all__ = ["Source", "Position", "Project"]
+__all__ = ["Position", "Project", "Source"]
