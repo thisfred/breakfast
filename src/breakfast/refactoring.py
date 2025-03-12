3dc7f6894c2a96a0f932920657a8ee7c66b65afb
Eric Casteleijn <thisfred@gmail.com>
2025-03-11 18:18:39 -0700

    Refactor

--- src/breakfast/refactoring.py
+++ src/breakfast/refactoring.py
@@ -431,12 +431,17 @@ class InlineCall:
             if (return_range := self.source.node_range(r))
         ]
 
-        if not return_ranges:
-            indentation = self.text_range.start.indentation
-            body = indent(
-                dedent(NEWLINE.join(line for line in new_lines) + NEWLINE),
-                indentation,
+        indentation = self.text_range.start.indentation
+        for return_range in return_ranges:
+            offset = return_range.start.row - body_range.start.row
+            new_lines[offset] = new_lines[offset].replace(
+                "return ", f"{self.name} = ", 1
             )
+        body = indent(
+            dedent(NEWLINE.join(new_lines) + NEWLINE),
+            indentation,
+        )
+        if not return_ranges:
             return (
                 Edit(
                     call_range,
@@ -444,26 +449,16 @@ class InlineCall:
                 ),
             )
 
-        for return_range in return_ranges:
-            offset = return_range.start.row - body_range.start.row
-            new_lines[offset] = new_lines[offset].replace(
-                "return ", f"{self.name} = ", 1
-            )
-        indentation = self.text_range.start.indentation
         insert_range = TextRange(
             self.text_range.start.line.start, self.text_range.start.line.start
         )
-        body = indent(
-            dedent(NEWLINE.join(new_lines) + NEWLINE),
-            indentation,
-        )
-        replace = Edit(call_range, text=self.name)
+
         return (
             Edit(
                 insert_range,
                 text=f"{body}",
             ),
-            replace,
+            Edit(call_range, text=self.name),
         )
 
     def get_new_lines(
